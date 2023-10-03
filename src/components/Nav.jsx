4bb74abf6145af969db521075e23420cdda1f9e6
Pranaav <67396569+Pranaav003@users.noreply.github.com>
2023-10-03 10:48:24 -0400

    Add files via upload

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <header className="px-8 py-6 absolute z-10 w-full font-inter font-bold text-lg">  
+      Made By Pranaav.
+      <Nav>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+
+        </ul>
+      </Nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import Hero from './Hero';
+import Blog from './Blog';
+import Footer from './Footer';
+
+export {
+    Hero,
+    Blog,
+    Footer
+}
